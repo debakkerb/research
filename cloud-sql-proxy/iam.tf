b4f69493499da231b796f228aaac7901e9931a36
Bjorn De Bakker <debakkerbjorn@gmail.com>
2021-06-06 14:24:11 +0100

    Adding enough permissions to retrieve the instance details and be able to connect to the instance

--- cloud-sql-proxy/iam.tf
+++ cloud-sql-proxy/iam.tf
@@ -21,14 +21,16 @@ resource "google_service_account" "sql_proxy_service_account" {
 }
 
 resource "google_service_account_iam_member" "proxy_service_account_access" {
-  member             = var.identity
+  for_each           = var.proxy_access_identities
+  member             = each.value
   role               = "roles/iam.serviceAccountUser"
   service_account_id = google_service_account.sql_proxy_service_account.id
 }
 
 resource "google_iap_tunnel_instance_iam_member" "id_iap_access" {
+  for_each = var.proxy_access_identities
   project  = module.cloud_sql_proxy_service_project.project_id
-  member   = var.identity
+  member   = each.value
   role     = "roles/iap.tunnelResourceAccessor"
   instance = google_compute_instance.proxy_instance.name
   zone     = var.zone
@@ -41,27 +43,28 @@ resource "google_project_iam_member" "cloud_sql_admin" {
 }
 
 resource "google_compute_instance_iam_member" "retrieve_and_update_instance_details" {
-  count         = var.enable_ssh_access ? 1 : 0
+  for_each      = var.proxy_access_identities
   project       = module.cloud_sql_proxy_service_project.project_id
   instance_name = google_compute_instance.proxy_instance.name
-  member        = var.identity
+  member        = each.value
   role          = var.create_custom_compute_get_role ? google_project_iam_custom_role.instance_details_role.0.id : "roles/compute.instanceAdmin.v1"
   zone          = var.zone
 }
 
 resource "google_project_iam_custom_role" "instance_details_role" {
-  count       = (var.create_custom_compute_get_role && var.enable_ssh_access) ? 1 : 0
+  count       = var.create_custom_compute_get_role ? 1 : 0
   project     = module.cloud_sql_proxy_service_project.project_id
-  permissions = ["compute.instances.get", "compute.instances.setMetadata"]
+  permissions = ["compute.instances.get", "compute.instances.setMetadata", "compute.projects.get"]
   role_id     = "computeInstanceDetailsLogin"
   title       = "Compute Instance Details Login"
   description = "Role to retrieve the instances details and to set instance metadata, required for updating SSH keys or RDP passwords and to login via gcloud compute ssh."
 }
 
 resource "google_compute_instance_iam_member" "oslogin_admin" {
-  count         = var.enable_ssh_access ? 1 : 0
+  for_each      = var.enable_ssh_access ? var.proxy_access_identities : []
   project       = module.cloud_sql_proxy_service_project.project_id
   instance_name = google_compute_instance.proxy_instance.name
-  member        = var.identity
+  member        = each.value
   role          = "roles/compute.osAdminLogin"
+  zone          = var.zone
 }
